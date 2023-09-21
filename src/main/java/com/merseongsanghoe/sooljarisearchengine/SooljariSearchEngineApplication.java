19db2aa4a904d258c718437c45a69b71b403516c
rhyun9584 <rhyun9584@naver.com>
2023-09-21 19:38:59 +0900

    real initial commit

---
+++ src/main/java/com/merseongsanghoe/sooljarisearchengine/SooljariSearchEngineApplication.java
@@ -0,0 +1,13 @@
+package com.merseongsanghoe.sooljarisearchengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SooljariSearchEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SooljariSearchEngineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/merseongsanghoe/sooljarisearchengine/SooljariSearchEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.merseongsanghoe.sooljarisearchengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SooljariSearchEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
